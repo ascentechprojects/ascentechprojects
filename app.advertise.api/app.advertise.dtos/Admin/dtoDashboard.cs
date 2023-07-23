992240e7549d41f791b130005550356893b0116c
PandeyM9068 <135303257+PandeyM9068@users.noreply.github.com>
2023-07-23 10:59:23 +0530

    dtoDashboardPrabhagOverview1

--- app.advertise.api/app.advertise.dtos/Admin/dtoDashboard.cs
+++ app.advertise.api/app.advertise.dtos/Admin/dtoDashboard.cs
@@ -1,22 +1,31 @@
 ﻿namespace app.advertise.dtos.Admin
 {
-    public class dtoDashboard:dtoBase
+    public class dtoDashboard : dtoBase
     {
-        public dtoDashboardStatus ApplicationStatus {get;set;}
-        public IEnumerable<dtoDashboardPrabhagOverview> PrabhagOverview { get;set;}
+        public dtoDashboardStatus ApplicationStatus { get; set; }
+        public IEnumerable<dtoDashboardPrabhagOverview> PrabhagOverview { get; set; }
     }
 
 
-    public class dtoDashboardStatus :dtoBase
+    public class dtoDashboardStatus : dtoBase
     {
         public int StatusFlag_Pending { get; set; }
         public int StatusFlag_Rejected { get; set; }
         public int StatusFlag_Closed { get; set; }
-        public int StatusFlag_Approved { get;set; }
+        public int StatusFlag_Approved { get; set; }
     }
     public class dtoDashboardPrabhagOverview : dtoBase
     {
         public string AppliStatusFlag { get; set; }
         public int StatusFlagTotal { get; set; }
+    }
+
+    public class dtoDashboardPrabhagOverview1 : dtoBase
+    {
+        //prabhag name from dtobase
+        public int Total { get; set; }
+        public int Pending { get; set; }
+        public int Sanction { get; set; }
+        public int Expired { get; set; }
     }
 }
