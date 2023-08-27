6a7fca220d75c392e0d85ab8fc828d2674a50123
PandeyM9068 <135303257+PandeyM9068@users.noreply.github.com>
2023-08-27 15:59:16 +0530

    auth application

--- app.advertise.api/app.advertise.libraries/UserRequestHeaders.cs
+++ app.advertise.api/app.advertise.libraries/UserRequestHeaders.cs
@@ -4,9 +4,9 @@
     public sealed class UserRequestHeaders
     {
         public string IpAddress { get; internal set; }
-        public string UserId { get; internal set; }= "WT_BNCMC1_v";
+        public string UserId { get; internal set; }
         public string Source { get; internal set; } = UserSource.Web.ToString();
-        public int UlbId { get; internal set; }=1;
+        public int UlbId { get; internal set; }
         public string HostAddress { get; internal set; }
         public string DeptId { get;internal set;}
     }
